8480541417f32291ae60241072c05e6be3cba176
Delio Marcos <deliomarcos.contato@gmail.com>
2022-01-10 20:54:19 -0300

    Ajustando aplicacao;

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 
 const chatbotDivs = document.querySelectorAll(".chatbot-serra-data")
 
-chatbotDivs.forEach((chatBotDiv) => {
+chatbotDivs.forEach(chatBotDiv => {
   ReactDOM.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
     chatBotDiv
   )
-}
-)
-
+})
